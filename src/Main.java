a076906eaa8c5a7b7b3345421a710b2a5ba3a32e
Work <andrewdavidmccall@hotmail.com>
2023-09-18 11:41:59 +0100

    SeptemberSkyJava

--- src/Main.java
+++ src/Main.java
@@ -1,119 +1,62 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception {
 
-        // TODO (Primitive, Class) (Pass-by-value, Pass-by-reference)
-        int myNumber = -25;
+        // Calculator e.g
+//        Calculator(1, '+', 2);  // = 3
+//        Calculator(4, '-', 2);  // = 2
+//        Calculator(12, '*', 2); // = 24
+//        Calculator(8, '/', 2);  // = 4
 
-//        myNumber = myNumber + 10;
-
-        System.out.println(myNumber);
-
-        // TODO Switch
-        if (myNumber > 10) {
-            System.out.println("Greater than ten");
-        } else if (myNumber >= 5) {
-            System.out.println("Greater or equal to five");
-        }else if (myNumber == 1 || (myNumber < 0 && myNumber > -50)){
-            System.out.println("Combined");
-        }else{
-            System.out.println(myNumber);
-        }
-
-
-        myNumber = 2;
-
-        switch (myNumber){
-
-            case 1:
-                System.out.println("one");
-                break;
-
-            case 2:
-                System.out.println("Two");
-                break;
-
-            case 3:
-                System.out.println("Three");
-                break;
-
-            default:
-                System.out.println(myNumber);
-                break;
-
-        }
-
-        myNumber = 5;
-
-        while (myNumber > 0){
-            System.out.println(myNumber);
-            myNumber = myNumber - 1;
-        }
-
-        myNumber = -5;
-
-        do{
-            System.out.println(myNumber);
-            myNumber = myNumber - 1;
-        }while (myNumber > 0);
-
-        for (int i = 1; i <= 10; i++){
-            System.out.println(i);
-        }
-
-        myNumber = 1;            // = 1
-        myNumber++;              // = 2
-        myNumber = myNumber + 3; // = 5
-        myNumber += 2;           // = 7
-
-        myNumber--;
-        myNumber = myNumber - 2;
-        myNumber -= 3;
-
-        // myNumber = 1
-
-        myNumber /= 1.01;           // 1/1.01= 0.99 = 0
-        myNumber *= 4;           // 0*4= 0
-
-        System.out.println(myNumber);
-
-        // Primitive Data Type - Can't be broken down any further
-        // double  64 bits
-        // float   32 bits
-
-        // long    64 bits
-        // int     32 bits
-        // short   16 bits
-
-        // byte    8 bits
-        // char    16bits Unicode
-
-        // boolean 1 bit
-
-
-        // Non primitive
-        // Strings
-        // Vectors
-        // Points
-        // Main
-        // Scanners
-
+        // 1 - Create a calculator method which takes 2 numbers and a condition
+        // 2 - Print the answer
+        // Extensions
+        // 3 - Add % and ^ (modlus and power) operations
+        // 4 - Add a text input to allow inputs for the numbers
+        // 5 - Add a text input to allow input for the operation
         Scanner s = new Scanner(System.in);
+        double a = s.nextDouble();
+        double b = s.nextDouble();
+        char o = s.next().charAt(0);
 
-        System.out.println(s.next());
-        // TODO Exceptions
-        myNumber = s.nextInt();
+        System.out.println(Calculator(a, o, b));
 
-        if (myNumber % 2 == 0){
-            System.out.println("Even");
-        }else{
-            System.out.println("Odd");
+//        System.out.println(Calculator(10, '+', 20));
+//        System.out.println(Calculator(10, '-', 20));
+//        System.out.println(Calculator(10, '*', 20));
+//        System.out.println(Calculator(10, '/', 20));
+//
+////        System.out.println(Calculator(10, 'f', 0));
+//        System.out.println(Calculator(10, '^', 2));
+//        System.out.println(Calculator(10, '%', 20));
+
+    }
+
+
+    private static double Calculator(double numberOne, char operation, double numberTwo) throws Exception {
+        switch (operation){
+            case '+':
+                return numberOne+numberTwo;
+            case '-':
+                return numberOne-numberTwo;
+            case '*':
+                return numberOne*numberTwo;
+            case '/':
+                if (numberTwo == 0){
+                    throw new Exception("Can't divide by zero!");
+                }
+                return numberOne/numberTwo;
+            case '^':
+                return Math.pow(numberOne, numberTwo);
+            case '%':
+                if (numberTwo == 0){
+                    throw new Exception("Can't modulo by zero!");
+                }
+                return numberOne%numberTwo;
         }
 
-
-
+        throw new Exception("Invalid Operation");
     }
 
 }
