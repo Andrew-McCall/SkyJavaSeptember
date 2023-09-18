4d42200143d69bcb0ac5820a09cc7103e4e0b0cd
Work <andrewdavidmccall@hotmail.com>
2023-09-18 15:33:48 +0100

    Example One

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,20 @@
-public class Driver {
+public class Driver extends Person {
 
     private Car car;    // car can be null
 //    private int number; // primitives always have a value
 
-    public Driver(){}
+    public Driver(){
+        super("Andrew", 2);
+    }
+
+    @Override
+    public String getName(){
+        return "Driver " + super.getName();
+    }
+
+    public Driver(String name, int age){
+        super(name, age);
+    }
 
     public double DriveCar(double miles){
         if (this.car == null) {
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main { // Everything secretly inherts from Object
     public static void main(String[] args) {
 
         // 1 // Car Class
@@ -24,6 +24,14 @@ public class Main {
         // 5 // Give Car a position which is edited by drive
         // 6 // ToString, Equals, HashCode
 
+        // Part 2
+        // Car Parent Class
+        // 1 // Create two child classes that extend Car: Supercar, Convertible
+        // 2 // Give the two classes unique behaviour
+        /// Extension - BREAK POLYMORPHISM
+        // 3 // Driver treat the child classes differently - use the sunroof. put super can in sport mode
+
+
         Car c1 = new Car("Black", "ABC", "Leaf", "Nissan", 45233);
         Car c2 = new Car("DFE");
 
@@ -41,5 +49,22 @@ public class Main {
 
         System.out.println(c1.getPosition());
 
+        Main m = new Main();
+        System.out.println(m.toString());
+        System.out.println(((Object) m).toString());
+
+        Object o = new Main();
+        System.out.println(o.toString());
+
+        System.out.println(driver.getName());
+        driver.Birthday();
+        System.out.println(driver.getAge());
+
     }
+
+    @Override
+    public String toString() {
+        return "Maine";
+    }
+
 }
