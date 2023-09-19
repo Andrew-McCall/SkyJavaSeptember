71e6d9b11a24f5d50a03c264a3b0bbb339c99772
Work <andrewdavidmccall@hotmail.com>
2023-09-19 17:06:23 +0100

    Static Extension

--- src/main/java/org/qa/skytdd/BankAccount.java
+++ src/main/java/org/qa/skytdd/BankAccount.java
@@ -1,27 +1,39 @@
 package org.qa.skytdd;
 
 public class BankAccount {
+
+    public static int AccountCounter;
+
     private int AccountNumber;
     private double Balance;
     private String AccountName;
 
+    public BankAccount(){
+        this(AccountCounter++);
+    }
+
     public BankAccount(int AccountNumber){
-        this.AccountNumber = AccountNumber;
-        this.AccountName = "Account "+ AccountNumber;
+        this(AccountNumber, 0.0);
     }
 
     public BankAccount(int accountNumber, double balance) {
-        AccountNumber = accountNumber;
-        Balance = balance;
+        this(accountNumber,balance,"Account "+ accountNumber);
     }
 
     public BankAccount(int accountNumber, double balance, String accountName) {
         AccountNumber = accountNumber;
         Balance = balance;
         AccountName = accountName;
+
+        if (accountNumber > AccountCounter){
+            AccountCounter = accountNumber + 1;
+        }
     }
 
-    public double RemoveFunds(double amount){
+    public double RemoveFunds(double amount) throws Exception{
+        if (this.Balance-amount < 0){
+            throw new Exception("You can't have minus funds!");
+        }
         this.Balance -= amount;
         return this.Balance;
     }
